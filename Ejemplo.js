49dbc3883b4f3b4e235d829e9dad2f9ca72b235c
DonDiabeton <102296807+DonDiabeton@users.noreply.github.com>
2022-04-12 10:58:56 -0400

    Add files via upload

---
+++ Ejemplo.js
@@ -0,0 +1,9 @@
+function muestraInformacionContacto () {<font></font>
+var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "MO", 12345] ;<Font></Font>
+Console.log (auntContactInfo) ;<Font></Font>
+}
+ /*
+ Variables         Datos
+ (auntContactInfo) =   ["Paula", "Smith", "Calle Principal 1234", "MO", 12345];
+/*
+    
